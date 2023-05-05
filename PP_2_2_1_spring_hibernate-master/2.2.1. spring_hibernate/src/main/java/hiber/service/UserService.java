9cc9b8978fb46b02ffdf8747872666b5f2764340
RyanEnd19 <olisov_vbk@bk.ru>
2023-05-05 19:15:25 +0300

    Done

---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,11 @@
+package hiber.service;
+
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void addUser(User user);
+    List<User> getListUsers();
+    List<User> getUserByCarModelAndSeries(String model, int series);
+}
